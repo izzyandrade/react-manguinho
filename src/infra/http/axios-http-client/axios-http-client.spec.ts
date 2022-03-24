bafdd3af9d493bfe615d731a4f18a4d9d80c44f5
izzyandrade <israelcarneiro97@gmail.com>
2022-03-23 21:25:27 -0300

    test: ensure axios is returning correct body on post

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -5,6 +5,11 @@ import { HttpPostParams } from "@/data/protocols/http";
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxiosResult = {
+  status: faker.datatype.number(),
+  data: faker.random.word(),
+};
+mockedAxios.post.mockResolvedValue(mockedAxiosResult);
 
 const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
@@ -23,5 +28,14 @@ describe("AxiosHttpClient", () => {
     const sut = makeSut();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  test("Should return correct body from Axios post response", async () => {
+    const sut = makeSut();
+    const response = await sut.post(mockPostRequest());
+    expect(response).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data,
+    });
   });
 });
