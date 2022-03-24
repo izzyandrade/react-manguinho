e719ac9945fface2e14306b3a6634dc7ff7879d3
izzyandrade <israelcarneiro97@gmail.com>
2022-03-23 21:25:04 -0300

    feat: ensure axios is returning correct body on post

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,16 @@
-import { HttpPostParams } from "@/data/protocols/http";
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+} from "@/data/protocols/http";
 import axios from "axios";
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const result = await axios.post(params.url, params.body);
+    return {
+      statusCode: result.status,
+      body: result.data,
+    };
   }
 }
