2b0bbbc23740fb46b7fd056365a7ff5432e51253
izzyandrade <israelcarneiro97@gmail.com>
2022-04-22 14:57:59 -0300

    test: prevent login to submit form if any fields present errors

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -138,4 +138,14 @@ describe("Login Component", () => {
     simulateValidSubmit(sut);
     expect(authenticationSpy.callsCount).toBe(1);
   });
+
+  test("Should not call Authentication if form is invalid", () => {
+    const { sut, authenticationSpy } = makeSut({
+      errorMessage: faker.random.words(),
+    });
+    populateEmailField(sut);
+    const form = sut.getByTestId("form");
+    fireEvent.submit(form);
+    expect(authenticationSpy.callsCount).toBe(0);
+  });
 });
