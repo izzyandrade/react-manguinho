2d2254ffd909d6272ab4718dddd71f7582486c9a
izzyandrade <israelcarneiro97@gmail.com>
2022-04-25 23:05:53 -0300

    feat: ensure email validation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import { InvalidFieldError } from "@/validation/errors";
 import { FieldValidation } from "@/validation/protocols/field-validation";
 
@@ -5,6 +6,8 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
   validate(value: string): Error | null {
-    return value && new InvalidFieldError();
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError();
   }
 }
