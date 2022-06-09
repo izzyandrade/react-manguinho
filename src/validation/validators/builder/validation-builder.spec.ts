9e3fe0ce29f46368b58333d8855078fd4a929937
izzyandrade <israelcarneiro97@gmail.com>
2022-06-09 19:57:23 -0300

    test: ensure ValidationBuilder has MinLengthValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -2,6 +2,7 @@ import {
   RequiredFieldValidation,
   EmailValidation,
 } from "@/validation/validators";
+import { MinLengthValidation } from "../min-length/min-length-validation";
 import { ValidationBuilder as sut } from "./validation-builder";
 
 describe("ValidationBuilder", () => {
@@ -13,5 +14,10 @@ describe("ValidationBuilder", () => {
   test("Should return email validation when asked", () => {
     const validations = sut.field("any_field").email().build();
     expect(validations).toEqual([new EmailValidation("any_field")]);
+  });
+
+  test("Should return minLength validation when asked", () => {
+    const validations = sut.field("any_field").minLength(5).build();
+    expect(validations).toEqual([new MinLengthValidation("any_field", 5)]);
   });
 });
