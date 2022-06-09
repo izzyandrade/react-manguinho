d8b0d132b7a7869a8c51c397016ab69f2cefb44b
izzyandrade <israelcarneiro97@gmail.com>
2022-06-09 20:03:32 -0300

    test: ensure ValidationBuilder has all validators when asked

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -4,6 +4,7 @@ import {
 } from "@/validation/validators";
 import { MinLengthValidation } from "../min-length/min-length-validation";
 import { ValidationBuilder as sut } from "./validation-builder";
+import faker from "@faker-js/faker";
 
 describe("ValidationBuilder", () => {
   test("Should return required field validation when asked", () => {
@@ -19,5 +20,19 @@ describe("ValidationBuilder", () => {
   test("Should return minLength validation when asked", () => {
     const validations = sut.field("any_field").minLength(5).build();
     expect(validations).toEqual([new MinLengthValidation("any_field", 5)]);
+  });
+
+  test("Should return a list of validations when asked", () => {
+    const validations = sut
+      .field("any_field")
+      .required()
+      .email()
+      .minLength(5)
+      .build();
+    expect(validations).toEqual([
+      new RequiredFieldValidation("any_field"),
+      new EmailValidation("any_field"),
+      new MinLengthValidation("any_field", 5),
+    ]);
   });
 });
