44b7319cc48da6ca9df075b4687bbe9b3dbda3a1
izzyandrade <israelcarneiro97@gmail.com>
2022-06-09 20:06:46 -0300

    refactor: used faker generated values for tests

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -8,31 +8,37 @@ import faker from "@faker-js/faker";
 
 describe("ValidationBuilder", () => {
   test("Should return required field validation when asked", () => {
-    const validations = sut.field("any_field").required().build();
-    expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
+    const fieldName = faker.database.column();
+    const validations = sut.field(fieldName).required().build();
+    expect(validations).toEqual([new RequiredFieldValidation(fieldName)]);
   });
 
   test("Should return email validation when asked", () => {
-    const validations = sut.field("any_field").email().build();
-    expect(validations).toEqual([new EmailValidation("any_field")]);
+    const fieldName = faker.database.column();
+    const validations = sut.field(fieldName).email().build();
+    expect(validations).toEqual([new EmailValidation(fieldName)]);
   });
 
   test("Should return minLength validation when asked", () => {
-    const validations = sut.field("any_field").minLength(5).build();
-    expect(validations).toEqual([new MinLengthValidation("any_field", 5)]);
+    const fieldName = faker.database.column();
+    const length = faker.datatype.number();
+    const validations = sut.field(fieldName).minLength(length).build();
+    expect(validations).toEqual([new MinLengthValidation(fieldName, length)]);
   });
 
   test("Should return a list of validations when asked", () => {
+    const fieldName = faker.database.column();
+    const length = faker.datatype.number();
     const validations = sut
-      .field("any_field")
+      .field(fieldName)
       .required()
       .email()
-      .minLength(5)
+      .minLength(length)
       .build();
     expect(validations).toEqual([
-      new RequiredFieldValidation("any_field"),
-      new EmailValidation("any_field"),
-      new MinLengthValidation("any_field", 5),
+      new RequiredFieldValidation(fieldName),
+      new EmailValidation(fieldName),
+      new MinLengthValidation(fieldName, length),
     ]);
   });
 });
