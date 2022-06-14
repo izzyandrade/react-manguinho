8a6359f605e7948174c93bc089b6b9e40436dbf0
izzyandrade <israelcarneiro97@gmail.com>
2022-06-13 22:17:26 -0300

    refactor: moved factories to their own files

---
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,7 @@
+import { RemoteAuthentication } from "@/data/usecases/authentication/remote-authentication";
+import { makeApiUrl } from "../../http/api-url-factory";
+import { makeHttpClient } from "../../http/axios-http-client-factory";
+
+export const makeRemoteAuthentication = (): RemoteAuthentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeHttpClient());
+};
