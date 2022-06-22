e7300c255ebfb96a7e05fba46445c55a1d2f80ac
izzyandrade <israelcarneiro97@gmail.com>
2022-06-21 21:40:43 -0300

    chore: removed main layer from test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.d.ts', '!<rootDir>/src/main/**/*'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
