2e584015da4d38b32a5774bb034366362adb8875
izzyandrade <israelcarneiro97@gmail.com>
2022-11-28 22:05:08 -0300

    test: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/usecases/saveAccessToken/local-save-access-token.spec.ts
+++ src/data/usecases/saveAccessToken/local-save-access-token.spec.ts
@@ -1,6 +1,7 @@
-import { LocalSaveAccessToken } from "./local-save-access-token";
 import { SetStorageMock } from "@/data/test/mock-cache";
+import { UnexpectedError } from "@/domain/error";
 import faker from "@faker-js/faker";
+import { LocalSaveAccessToken } from "./local-save-access-token";
 
 type SutTypes = {
   sut: LocalSaveAccessToken;
@@ -32,5 +33,11 @@ describe("LocalSaveAccessToken", () => {
     });
     const promise = sut.save(faker.datatype.uuid());
     await expect(promise).rejects.toThrow(new Error());
+  });
+
+  test("Should throw exception if accessToken is falsy", async () => {
+    const { sut } = makeSut();
+    const promise = sut.save(undefined);
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
