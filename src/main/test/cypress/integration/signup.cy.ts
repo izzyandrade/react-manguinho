a2a78b2b69755d29d9b65539c4fd9874f8ef238b
izzyandrade <israelcarneiro97@gmail.com>
2022-11-30 21:07:38 -0300

    test: ensure signup fields show error state if form is invalid

--- src/main/test/cypress/integration/signup.cy.ts
+++ src/main/test/cypress/integration/signup.cy.ts
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { testInputStatus } from "../support/helpers";
 
 describe("Signup", () => {
@@ -10,6 +11,19 @@ describe("Signup", () => {
     testInputStatus("name", "Campo Obrigatório");
     testInputStatus("password", "Campo Obrigatório");
     testInputStatus("passwordConfirmation", "Campo Obrigatório");
+    cy.getByTestId("submit-button").should("have.attr", "disabled");
+    cy.getByTestId("error-wrap").should("not.have.descendants");
+  });
+
+  it("Should present error state if form is invalid", () => {
+    cy.getByTestId("email").type(faker.random.word());
+    cy.getByTestId("name").type(faker.random.alphaNumeric(3));
+    cy.getByTestId("password").type(faker.random.alphaNumeric(3));
+    cy.getByTestId("passwordConfirmation").type(faker.random.alphaNumeric(3));
+    testInputStatus("email", "Campo inválido");
+    testInputStatus("name", "Campo inválido");
+    testInputStatus("password", "Campo inválido");
+    testInputStatus("passwordConfirmation", "Campo inválido");
     cy.getByTestId("submit-button").should("have.attr", "disabled");
     cy.getByTestId("error-wrap").should("not.have.descendants");
   });
